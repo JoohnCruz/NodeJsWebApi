38ffe24b5876fda49993ca9e5f3768ca722cd44d
Jonathan Cruz Rocha Ferreira <jonathan.ferreira@animaeducacao.com.br>
2015-11-03 18:34:56 -0200

    User Routes

--- server.js
+++ server.js
@@ -18,6 +18,64 @@ router.get('/', function(req, res){
 	res.json({ message: 'OK' });
 });
 
+var UserRoute = router.route('/users');
+UserRoute.post(function(req, res){
+
+	var user = new User();
+	user.name = req.body.name;
+	user.save(function(error){
+		if(error)
+			res.send(error);
+
+		res.json({ message: 'Usuario criado!', data: user });
+	});
+});
+UserRoute.get(function(req, res) {
+	User.find(function (err, users) {
+		if (err)
+			res.send(err);
+
+		res.json(users);
+	});
+});
+
+
+var UserRouteId = router.route('/users/:user_id');
+UserRouteId.get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+        if (err)
+            res.send(err);
+
+        res.json(user);
+    });
+});
+
+UserRouteId.put(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err)
+                res.send(err);
+
+        user.name = req.body.name;
+
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Usuario Atualizado!' });
+        });
+    });
+});
+
+UserRouteId.delete(function(req, res) {
+    User.remove({
+        _id: req.params.user_id
+    }, function(err, user) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Usuario Deletado' });
+    });
+});
 
 app.use('/api', router);
 app.listen(port);
